174dec6cbc3b1d59419c2de27539e5f08dd74bcc
Vlad Rusakov <vlad.rusakov.1996@mail.ru>
2019-09-15 21:05:46 +0300

    first version with ugly conditions and some physics bugs

--- src/index.js
+++ src/index.js
@@ -22,6 +22,9 @@ function draw(tFrame) {
 
     drawPlatform(context)
     drawBall(context)
+    drawTextInfo(context)
+    if(gameState.bonus.isActive)
+        drawBonus(context)
 }
 
 function update(tick) {
@@ -29,9 +32,64 @@ function update(tick) {
     const vx = (gameState.pointer.x - gameState.player.x) / 10
     gameState.player.x += vx
 
-    const ball = gameState.ball
-    ball.y += ball.vy
-    ball.y += ball.vx
+    const ball = gameState.ball;
+    const player = gameState.player
+    if(gameState.isLose)
+        stopGame(gameState.stopCycle)
+
+     if(ball.y+ball.radius >= canvas.height)
+     {
+        clearInterval(gameState.timerScore)
+        clearInterval(gameState.timerSpeed)
+        clearInterval(gameState.timerBonus)
+        gameState.isLose = true;
+     }
+
+    if(ball.x+ball.radius >= canvas.width || ball.x-ball.radius <= 0)
+        ball.vx *= -1
+    if(ball.y+ball.radius >= canvas.height || ball.y-ball.radius <= 0)
+        ball.vy *= -1
+    
+    if( (ball.x-ball.radius+ball.vy >= player.x-player.width/2 &&
+        ball.x+ball.radius <= player.x+player.width/2) &&
+        (ball.y+ball.radius >= player.y-player.height/2)
+            )
+            {
+                ball.vy *= -1
+            }
+
+    else if(ball.y+ball.radius >= player.y-player.height/2 &&
+    (
+        (ball.x+ball.radius >= player.x-player.width/2 && 
+            ball.x+ball.radius <= player.x+player.width/2) ||
+        (ball.x-ball.radius >= player.x-player.width/2 && 
+            ball.x-ball.radius <= player.x+player.width/2) 
+    ))
+    {
+        ball.vx *= -1
+    }
+        
+        ball.x += ball.vx
+        ball.y += ball.vy
+
+    const bonus = gameState.bonus;
+
+    if(gameState.bonus.isActive)
+    {
+        if(bonus.top + bonus.height >= player.y-player.height/2 &&
+            (
+                (bonus.left+bonus.width >= player.x-player.width/2 && 
+                    bonus.left+bonus.width <= player.x+player.width/2) ||
+                (bonus.left >= player.x-player.width/2 && 
+                    bonus.left <= player.x+player.width/2) 
+            ))
+            {
+                bonus.isActive = false;
+                gameState.score += 15
+            }
+        gameState.bonus.left += gameState.bonus.vx
+        gameState.bonus.top += 5
+    }
 }
 
 function run(tFrame) {
@@ -71,6 +129,52 @@ function drawBall(context) {
     context.closePath();
 }
 
+function drawBonus(context)
+{
+    const {left, top, width, height} = gameState.bonus
+    const coordsArray = [
+        [left+width/3, top],
+        [left+width*2/3, top],
+        [left+width*2/3, top+height/3],
+        [left+width, top+height/3],
+        [left+width, top+height*2/3],
+        [left+width*2/3, top+height*2/3],
+        [left+width*2/3, top+height],
+        [left+width/3, top+height],
+        [left+width/3, top+height*2/3],
+        [left, top+height*2/3],
+        [left, top+height/3],
+        [left+width/3, top+height/3],
+    ]
+    context.beginPath();
+    context.moveTo(left+width/3, top)
+    coordsArray.forEach(point => {
+        context.lineTo(point[0], point[1])
+    });
+    context.fillStyle = "#00F00F";
+    context.fill();
+    context.closePath();
+}
+
+function generateBonusParams()
+{
+    gameState.bonus.left = Math.random() * (canvas.width - gameState.bonus.width)
+    gameState.bonus.top = Math.random() * (canvas.height/2 - gameState.bonus.height)
+}
+
+function drawTextInfo(context)
+{
+    if(gameState.isLose)
+    {
+        context.fillStyle = "#FF0000"
+        context.font = "48px serif"
+        context.fillText("You lose", canvas.width*5/12, canvas.height/2)
+    }
+    context.fillStyle = "#FF00FF"
+    context.font = "48px serif"
+    context.fillText("SCORE: " + gameState.score, canvas.width/10, canvas.height/10)
+}
+
 function setup() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -98,11 +202,42 @@ function setup() {
     };
     gameState.ball = {
         x: canvas.width / 2,
-        y: 0,
+        y: 26,
         radius: 25,
-        vx: 0,
+        vx: 5,
         vy: 5
     }
+    gameState.bonus = {
+        top: 0,
+        left: 0,
+        width: 30,
+        height: 30,
+        vx: 0,
+        vy: 0,
+        angle:0,
+        t: 0,
+        isActive: false
+    }
+
+    gameState.isLose = false
+    gameState.score = 0
+
+    gameState.timerScore = setInterval(function(){gameState.score++}, 1000);
+    gameState.timerSpeed = setInterval(function()
+    {
+        if(gameState.player.width > 200)
+            gameState.player.width-=10
+        gameState.ball.vx *= 1.1
+        gameState.ball.vy *= 1.1
+    }, 
+    30000);
+    gameState.timerBonus = setInterval(function()
+    {   
+        gameState.player.width+=15
+        gameState.bonus.isActive = true;
+        generateBonusParams()
+    }, 
+    15000);
 }
 
 setup();
